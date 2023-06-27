26fad651eebc20a7eeafec53f6884e814996aa0a
hannes <hannesdelbeke@gmail.com>
2023-06-27 20:59:42 +0100

    refactor main so a method applies stylesheet

--- unreal_qt_stylesheet/main.py
+++ unreal_qt_stylesheet/main.py
@@ -1,27 +1,61 @@
-import sys
+# import sys
 import os
 
-from Qt import QtWidgets, QtCore, QtGui
-from Qt import _loadUi
+
+try:
+    from PySide2 import QtWidgets, QtCore, QtGui
+except ImportError:
+    pass
+
+try:
+    from PyQt5 import QtWidgets, QtCore, QtGui
+except ImportError:
+    pass
+
+try:
+    from PySide6 import QtWidgets, QtCore, QtGui
+except ImportError:
+    pass
+
+try:
+    from PyQt6 import QtWidgets, QtCore, QtGui
+except ImportError:
+    pass
+
+# from Qt import QtWidgets, QtCore, QtGui
+# from Qt import _loadUi
+from pathlib import Path
 
 
-MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
-UI_PATH = os.path.join(MODULE_PATH, 'ui', 'progress.ui')
-QSS_PATH = os.path.join(MODULE_PATH, 'ue.qss')
+# MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
+# UI_PATH = os.path.join(MODULE_PATH, 'ui', 'progress.ui')
+# QSS_PATH = os.path.join(MODULE_PATH, 'ue.qss')
+MODULE_PATH = Path(os.path.abspath(__file__)).parent
+QSS_PATH = MODULE_PATH / 'ue.qss'
 
 
-if __name__ == '__main__':
+def style_qapplication():
+    """ Apply the Unreal dark stylesheet to the current QApplication """
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
-    app = QtWidgets.QApplication(sys.argv)
-
-    QtCore.QResource.registerResource("icons.rcc")
-
+    app = QtWidgets.QApplication.instance()
+    QtCore.QResource.registerResource(str(MODULE_PATH / "icons.rcc") )
     with open(QSS_PATH, 'r') as f:
         qss = f.read()
         app.setStyleSheet(qss)
 
-    window = QtWidgets.QMainWindow()
-    _loadUi(UI_PATH, window)
-    window.show()
 
-    sys.exit(app.exec_())
+# if __name__ == '__main__':
+#     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+#     app = QtWidgets.QApplication(sys.argv)
+#
+#     QtCore.QResource.registerResource("icons.rcc")
+#
+#     with open(QSS_PATH, 'r') as f:
+#         qss = f.read()
+#         app.setStyleSheet(qss)
+#
+#     window = QtWidgets.QMainWindow()
+#     _loadUi(UI_PATH, window)
+#     window.show()
+#
+#     sys.exit(app.exec_())
